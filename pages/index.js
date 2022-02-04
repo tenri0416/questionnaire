0739a9ca1332581804d31af712db707c2497d5e5
tenri0416 <tenriyu0416@gmail.com>
2022-02-04 22:53:08 +0900

    アンケート

---
+++ pages/index.js
@@ -0,0 +1,153 @@
+
+import { useForm, Controller } from 'react-hook-form'
+import { Container } from '@material-ui/core'
+import Input from "@material-ui/core/Input"
+
+import { getFirestore, collection, addDoc } from 'firebase/firestore'
+
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBqTNZSVEHxwYWfjwNIvyJOPf66JUUQ5vw",
+  authDomain: "questionnaire-d9d5d.firebaseapp.com",
+  projectId: "questionnaire-d9d5d",
+  storageBucket: "questionnaire-d9d5d.appspot.com",
+  messagingSenderId: "564314984088",
+  appId: "1:564314984088:web:a4f9a6dced1326d879caed"
+};
+
+
+initializeApp(firebaseConfig)
+
+
+
+export default function Home() {
+  const { watch, register, handleSubmit, formState: { errors }, control } = useForm()
+
+
+  const watchIs = watch('isLearning')
+  const watchWas = watch('wasLearning')
+
+  const onSubmit = (date) => {
+
+    const name = date.name
+    const birth = date.birth
+    const isLearning = date.isLearning
+    const wasLearning = date.wasLearning
+    const db = getFirestore()
+    const lang = date.lang
+
+    const docRef = addDoc(collection(db, "answers"), {
+      name: name,
+      birth: birth,
+      isLearning: isLearning,
+      wasLearning: wasLearning,
+      lang: lang
+    })
+    console.log("Document written with ID: ", docRef.birth);
+
+
+  }
+
+  return (
+    <>
+      <Container>
+        <h1>プログラミング学習に関するアンケート</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <label htmlFor="name">Q1. 名前を入力してください（匿名可）。</label>
+            <Controller
+              name="name"
+              defaultValue=""
+              control={control}
+              render={({ field: { value, onChange } }) =>
+                <Input value={value} onChange={onChange} />}
+            />
+          </div>
+
+          <div>
+            <label htmlFor="birth">Q2. 生年月日を入力してください。</label>
+            <Controller
+              name="birth"
+              defaultValue=""
+              control={control}
+              rules={{ required: true, pattern: /^[0-9]{8}$/ }}
+              render={({ field: { value, onChange } }) => <Input value={value} onChange={onChange} />}
+            />
+            {
+              errors.birth && errors.birth.type === "required" ?
+                <span>このフィールドは回答必須です。</span> :
+                null
+            }
+            {
+              errors.birth && errors.birth.type === "pattern" ?
+                <span>整数8桁で入力してください。</span> : null
+            }
+          </div>
+          <div>
+            <span>Q3. 現在、プログラミングを学習していますか？</span>
+            <input id="isLearning1"
+              {...register('isLearning', { required: true })}
+              name="isLearning"
+              type="radio"
+              value="true"
+            />
+            <label htmlFor='isLearning1'>はい</label>
+
+            <input id="isLearning2"
+              {...register('isLearning', { required: true })}
+              name="isLearning"
+              type="radio"
+              value="false" />
+            <label htmlFor='isLearning2'>いいえ</label>
+            {
+              errors.isLearning &&
+              <span>このフィールドは回答必須です</span>
+            }
+
+          </div>
+          <div>
+            <span>Q4. これまでに、プログラミングを学習したことがありますか？</span>
+            <input id="wasLearning1"
+              {...register('wasLearning', { required: true })}
+              name="wasLearning"
+              type="radio"
+              value="true" />
+            <label htmlFor='wasLearning1'>はい</label>
+            <input id="wasLearning2"
+              {...register('wasLearning', { required: true })}
+              name='wasLearning'
+              type="radio"
+              value="false" />
+            <label htmlFor="wasLearning2">いいえ</label>
+            {
+              errors.wasLearning &&
+              <span>このフィールドは回答必須です。</span>
+            }
+
+          </div>
+          {
+            watchIs === "true" || watchWas === "true" ?
+              < div >
+                <span>今まで学習したことあるプログラミング言語を全て教えて下さい。</span>
+
+                <textarea id="lang"{...register('lang')} />
+              </div> : null
+          }
+
+          <input type="submit" value="アンケートを提出する" />
+
+        </form >
+      </Container>
+    </>
+
+  )
+}
+
+//defaultValue=フォームの初期値
+//errors.name属性でエラーメッセージ
+//registerで入力されたデータが簡単に取得できる
+//required:trueでこの値は入力必須ですよを実装
+//pattenは正規表示を指定できる
